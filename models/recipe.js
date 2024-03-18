dca9b361b4616bd6c651dd99e1dd550b1a5f389a
Booker Ngoon <bookerngoon@gmail.com>
2024-03-18 15:26:04 -0400

    created model and restrict.js files

---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const Recipe = new Schema(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    includeIngredients: { type: String, required: true },
+    summary: { type: String, required: true },
+    insturctionsRequired: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("recipes", Recipe);
