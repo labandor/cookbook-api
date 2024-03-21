296b2c36ce43956a6530737deac61371b0d22172
Laband <lukeabandeen@gmail.com>
2024-03-21 15:09:31 -0400

    changed the required aspect of the schema

--- models/Recipe.js
+++ models/Recipe.js
@@ -6,9 +6,9 @@ const Recipe = new Schema(
     title: { type: String, required: true },
     image: { type: String, required: true },
     summary: { type: String, required: true },
-    sourceUrl: { type: String, required: true },
+    sourceUrl: { type: String, required: false },
     cuisine: { type: String, required: true },
-    steps: [{ type: String, required: true }],
+    steps: [{ type: String, required: false }],
     ingredients: [{ type: String, required: true }],
   },
   { timestamps: true }
