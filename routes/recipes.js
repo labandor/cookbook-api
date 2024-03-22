33ab597ca12fa250b2948a28597f6a73a7ae923e
Booker Ngoon <bookerngoon@gmail.com>
2024-03-22 10:43:03 -0400

    deploy to heroku

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,7 +5,6 @@ import restrict from "../helpers/restrict.js";
 const router = Router();
 
 router.get("/recipies", controllers.getRecipies);
-// router.get("recipes/:cuisines", controllers.getRecipesByCuisines);
 router.get("/recipies/:id", controllers.getRecipe);
 router.post("/recipies", restrict, controllers.createRecipe);
 router.put("/recipies/:id", restrict, controllers.updateRecipe);
